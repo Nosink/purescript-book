3507a4127c86ce9a7ddf53459b641deb6e5ca8ac
Rick :: Nosink Ð <ricardruizc@gmail.com>
2023-11-28 16:12:55 +0100

    Exercise 3 solved

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -7,25 +7,23 @@ import Data.List (filter, head, nubByEq, null)
 import Data.Maybe (Maybe)
 
 findEntryByStreet :: String -> AddressBook -> Maybe Entry
-findEntryByStreet street = head <<< filter (\entry -> entry.address.street == street) {- ilterEntryByStreet -}
-  {-
+findEntryByStreet street = head <<< filter filterEntryByStreet
   where
   filterEntryByStreet :: Entry -> Boolean
-  filterEntryByStreet entry = _.address.street entry == street 
-  -}
+  filterEntryByStreet e = e.address.street == street 
 
 isInBook :: String -> String -> AddressBook -> Boolean
-isInBook firstName lastName book = not $ null(filter (\entry -> entry.firstName == firstName && entry.lastName == lastName) {- filterEntry -} book)
-  {-
+isInBook firstName lastName = not null <<< filter filterEntry
   where
-    filterEntry :: String -> String -> Entry -> Boolean
-    filterEntry _ _ entry = _.firstName entry == firstName && _.lastName entry == lastName
-  -}
+  filterEntry :: Entry -> Boolean
+  filterEntry entry =
+     entry.firstName == firstName &&
+     entry.lastName  == lastName
 
 removeDuplicates :: AddressBook -> AddressBook
-removeDuplicates = nubByEq {- findDuplicate -} (\entry1 entry2 -> entry1.firstName == entry2.firstName && entry1.lastName == entry2.lastName)
-  {-
+removeDuplicates = nubByEq findDuplicate
   where
     findDuplicate :: Entry -> Entry -> Boolean
-    findDuplicate entry1 entry2 = entry1.firstName == entry2.firstName && entry1.lastName == entry2.lastName
-  -}
+    findDuplicate entry1 entry2 = 
+      entry1.firstName == entry2.firstName && 
+      entry1.lastName  == entry2.lastName
